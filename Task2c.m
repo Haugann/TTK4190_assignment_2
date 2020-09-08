c2945b737798ec6ba8501ae081c0caa022c8267c
Håkon Haugann <Haugann@dhcp-10-22-69-148.wlan.ntnu.no>
2020-09-08 13:40:09 +0200

    Add: Code for task 2c

---
+++ Task2c.m
@@ -0,0 +1,31 @@
+clear
+clc
+%% Variable declarations
+a1_phi = 2.87;
+a2_phi = -0.65;
+
+d_a_max = 30;
+e_phi_max = 15;
+zeta_phi = 0.707;
+omega_phi = sqrt(2 * abs(a2_phi));
+
+Kp_phi = d_a_max / e_phi_max * sign(a2_phi);
+Kd_phi = (2 * zeta_phi * omega_phi- a1_phi) / a2_phi;
+
+%% Transfer function
+H = tf(a2_phi, [1 (a1_phi + Kd_phi * a2_phi) (a2_phi * Kp_phi) 0]);
+
+K_i = linspace(0,10,10000); 
+[R, K] = rlocus(H, -K_i);
+
+%% Finding the max gain
+for i = 1:length(K)
+    if (real(R(1,i)) > 0) || (real(R(2,i)) > 0) || (real(R(3,i)) > 0)
+        K_i_max = K(i);
+        break
+    end
+end
+
+    
+
+
